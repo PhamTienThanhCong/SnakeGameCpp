4cc29fe6173d498722e774166aa23eefbbf75f4c
lenghia100703 <21020366@vnu.edu.vn>
2022-04-22 22:52:09 +0700

    sua

--- main.cpp
+++ main.cpp
@@ -1,203 +1,18 @@
 #include <iostream>
+#include <deque>
 #include <SDL.h>
-#include <cstdlib>
 using namespace std;
 
-const int buoc = 50;
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
+const int SCREEN_WIDTH = 500;
+const int SCREEN_HEIGHT = 500;
 const string WINDOW_TITLE = "Game snake";
-
-class food{
-private:
-    int x;
-    int y;
-public:
-    food(){
-        x = ramdomPointX();
-        y = ramdomPointY();
-    }
-    int ramdomPointX(){
-        return (rand() % (SCREEN_WIDTH/50))*50;
-    }
-    int ramdomPointY(){
-        return (rand() % (SCREEN_HEIGHT/50))*50;
-    }
-    void newPoint(){
-        x = ramdomPointX();
-        y = ramdomPointY();
-    }
-    int getX(){
-        return x;
-    }
-    int getY(){
-        return y;
-    }
-};
-
-class Snake{
-private:
-    int x;
-    int y;
-    int size_snake;
-public:
-    Snake(){
-        x = 0;
-        y = 0;
-        size_snake = 3;
-    }
-    int getX(){
-        return x;
-    }
-    int getY(){
-        return y;
-    }
-    void getKey(char key){
-        if (key == 'u'){
-            y -= buoc;
-        }else if (key == 'd'){
-            y += buoc;
-        }else if (key == 'l'){
-            x -= buoc;
-        }else if (key == 'r'){
-            x += buoc;
-        }
-    }
-    bool checkDie(){
-        if (x >= SCREEN_WIDTH || x < 0 || y >=SCREEN_HEIGHT || y < 0){
-            return false;
-        }
-        return true;
-    }
-    bool eat(food foodSnake){
-        if (foodSnake.getX() == x && foodSnake.getY() == y){
-            size_snake++;
-            return true;
-        }
-        return false;
-    }
-};
+const int jump = 25;
 
 void initSDL(SDL_Window* &window, SDL_Renderer* &renderer);
-
 void logSDLError(std::ostream& os, const std::string &msg, bool fatal = false);
-
 void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
-
 void waitUntilKeyPressed();
 
-SDL_Texture* loadTexture(string path, SDL_Renderer* renderer);
-
-SDL_Surface* IMG_Load(const char* path);
-
-int main(int argc, char* argv[])
-{
-     SDL_Window* window;
-     SDL_Renderer* renderer;
-     initSDL(window, renderer);
-
-     SDL_RenderClear(renderer);
-     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-
-     // vẽ hình chữ nhật
-     SDL_Rect filledRect;
-     filledRect.h = 50;
-     filledRect.w = 50;
-
-     SDL_Rect fillFood;
-     fillFood.h = 50;
-     fillFood.w = 50;
-
-//     waitUntilKeyPressed();
-     Snake mySnake;
-     food foodOfSnake;
-
-     SDL_Event e;
-     char key = 'r';
-
-     while (true) {
-        if ( SDL_PollEvent(&e)){
-            if (e.type == SDL_KEYDOWN) {
-                switch (e.key.keysym.sym){
-                    case SDLK_ESCAPE:
-                        {
-                            quitSDL(window, renderer);
-                            return 0;
-                        }
-                    case SDLK_UP:
-                        {
-                            key = 'u';
-                            break;
-                        }
-                    case SDLK_DOWN:
-                        {
-                            key = 'd';
-                            break;
-                        }
-                    case SDLK_LEFT:
-                        {
-                            key = 'l';
-                            break;
-                        }
-                    case SDLK_RIGHT:
-                        {
-                            key = 'r';
-                            break;
-                        }
-                }
-            }else if(e.type == SDL_QUIT){
-                quitSDL(window, renderer);
-                return 0;
-            }
-        }
-
-        if ( mySnake.eat(foodOfSnake) ){
-            foodOfSnake.newPoint();
-        }
-        if ( mySnake.checkDie() == false ){
-            return 0;
-        }
-
-        SDL_Delay(200);
-        mySnake.getKey(key);
-
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
-
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-
-        filledRect.x = mySnake.getX();
-        filledRect.y = mySnake.getY();
-        SDL_RenderFillRect(renderer, &filledRect);
-
-        SDL_SetRenderDrawColor(renderer, 255, 253, 7, 5);
-        fillFood.x = foodOfSnake.getX();
-        fillFood.y = foodOfSnake.getY();
-
-        SDL_RenderFillRect(renderer, &fillFood);
-
-        SDL_RenderPresent(renderer);
-
-     }
-
-//     quitSDL(window, renderer);
-     return 0;
-
-}
-
-
-SDL_Surface* IMG_Load(const char* path){
-
-}
-
-void logSDLError(std::ostream& os, const std::string &msg, bool fatal)
-{
-    os << msg << " Error: " << SDL_GetError() << std::endl;
-    if (fatal) {
-        SDL_Quit();
-        exit(1);
-    }
-}
 void initSDL(SDL_Window* &window, SDL_Renderer* &renderer)
 {
      if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
@@ -220,6 +35,14 @@ void initSDL(SDL_Window* &window, SDL_Renderer* &renderer)
      SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
      SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
 }
+void logSDLError(std::ostream& os, const std::string &msg, bool fatal)
+{
+    os << msg << " Error: " << SDL_GetError() << std::endl;
+    if (fatal) {
+        SDL_Quit();
+        exit(1);
+    }
+}
 
 void quitSDL(SDL_Window* window, SDL_Renderer* renderer)
 {
@@ -235,7 +58,167 @@ void waitUntilKeyPressed()
         if ( SDL_WaitEvent(&e) != 0 &&
             (e.type == SDL_KEYDOWN || e.type == SDL_QUIT) )
             return;
-        SDL_Delay(100);
+        SDL_Delay(0);
 
      }
+}
+
+class food{
+public:
+
+    int randomX(){
+        return (rand() % (SCREEN_WIDTH/25))*25;
+    }
+    int randomY(){
+        return (rand() % (SCREEN_HEIGHT/25))*25;
+    }
+    void newPoint(){
+        x = randomX();
+        y = randomY();
+    }
+    int x = randomX();
+    int y = randomY();
+    int getX(){
+        return x;
+    }
+    int getY(){
+        return y;
+    }
+};
+
+
+class snake{
+public:
+    int x = 250;
+    int y = 250;
+    int sizeSnake = 4;
+    int getX(){
+        return x;
+    }
+    int getY(){
+        return y;
+    }
+    void getKey(char key){
+        if (key == 'u'){
+            y -= jump;
+        }else if (key == 'd'){
+            y += jump;
+        }else if (key == 'l'){
+            x -= jump;
+        }else if (key == 'r'){
+            x += jump;
+        }
+    }
+    bool checkDie(){
+        if (x > SCREEN_WIDTH || x < 0 || y > SCREEN_HEIGHT || y < 0){
+            return false;
+        }
+        return true;
+    }
+    bool eatFood(food f){
+        if (x == f.getX() && y == f.getY()){
+            sizeSnake++;
+            return true;
+        }
+        return false;
+    }
+
+};
+
+void drawBox(int a, int b){
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+
+    SDL_Rect box = {a, b, 25, 25};
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderFillRect(renderer, &box);
+    SDL_RenderPresent(renderer);
+}
+
+
+
+int main(int argc, char* argv[]){
+     SDL_Window* window;
+     SDL_Renderer* renderer;
+     initSDL(window, renderer);
+     SDL_RenderClear(renderer);
+     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+     SDL_Event e;
+     snake mySnake;
+     food myFood;
+     char key = 'r';
+     drawBox(25, 25);
+
+
+     SDL_Rect headSnake;
+     headSnake.h = 25;
+     headSnake.w = 25;
+
+     SDL_Rect foodofSnake;
+     foodofSnake.h = 25;
+     foodofSnake.w = 25;
+
+
+
+
+
+
+
+
+     bool checkEventKey = true;
+     while (checkEventKey) {
+        while ( SDL_PollEvent(&e)){
+            if (e.type == SDL_KEYDOWN) {
+                if (e.key.keysym.sym == SDLK_UP && key != 'd') {key = 'u';}
+                if (e.key.keysym.sym == SDLK_DOWN && key != 'u') {key = 'd';}
+                if (e.key.keysym.sym == SDLK_LEFT && key != 'r') {key = 'l';}
+                if (e.key.keysym.sym == SDLK_RIGHT && key != 'l') {key = 'r';}
+            }
+            if(e.type == SDL_QUIT) checkEventKey = false;
+        }
+        mySnake.getKey(key);
+
+
+
+
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+
+
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        headSnake.x = mySnake.getX();
+        headSnake.y = mySnake.getY();
+        SDL_RenderFillRect(renderer, &headSnake);
+
+
+
+
+
+
+        if (mySnake.checkDie() == false) return 0;
+
+        if (mySnake.eatFood(myFood) == true) {
+            myFood.newPoint();
+
+        }
+
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+        foodofSnake.x = myFood.getX();
+        foodofSnake.y = myFood.getY();
+        SDL_RenderFillRect(renderer, &foodofSnake);
+
+
+        SDL_RenderPresent(renderer);
+        SDL_Delay(150);
+    }
+
+
+
+
+    quitSDL(window, renderer);
+    return 0;
 }
